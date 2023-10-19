1eeb6c483e4052beac30c3294ea461d97e65b88b
jieunw <jieunwang00@gmail.com>
2023-10-19 23:59:35 +0900

    ep2-1 강의내용 완

--- ep02-ver1/src/main.js
+++ ep02-ver1/src/main.js
@@ -1,7 +1,7 @@
 import test from "./test.json";
 
 // 여기 NODE_ENV는 빌드할 때 파일에 내용이 들어가지 않음.
-// tree shaking!
+// = tree shaking!
 
 async function getProducts() {
     if (process.env.NODE_ENV === "development") {
@@ -32,59 +32,117 @@ function sumAllCount(countMap) {
     });
     return sum;
 }
-
-async function main() {
-    const products = await getProducts();
-    const countMap = {};
-    console.log("products", products);
-    document.querySelector("#products").innerHTML = products
-        .map(
-            (product, index) => `
-  <div class="product" data-product-id="${product.id} data-product-index="${index}">
+function getProductHTML(product, count = 0) {
+    return `
+  <div class="product" data-product-id="${product.id}">
   <img src="${product.images[0]}" alt="Image of ${product.name}" />
   <p>${product.name}</p>
-  <div class="flex items-center justify-between"><span>Price: ${product.regularPrice}</span>
+  <div class="flex items-center justify-between"><span>Price: ${
+      product.regularPrice
+  }</span>
   <button type="button" class="btn-decrease  bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">-</button>
-  <span class="cart-count text-green-800"></span>
+  <span class="cart-count text-green-800">${count === 0 ? 0 : count}</span>
   <button type="button" class="btn-increase bg-green-200 hover:bg-green-300 text-green-800 py-1 px-3 rounded-full">+</button>
 
   </div>
-  </div>
+  </div>`;
+}
 
-  `
-        )
+async function main() {
+    const products = await getProducts();
+    const productMap = {};
+    products.forEach((product) => {
+        productMap[product.id] = product;
+    });
+    const countMap = {};
+
+    const updateProductCount = (productId) => {
+        const productElement = document.querySelector(
+            `.product[data-product-id='${productId}']`
+        );
+        const cartCount = productElement.querySelector(".cart-count");
+        cartCount.innerHTML = countMap[productId];
+        if (countMap[productId] === 0) {
+            cartCount.innerHTML = "";
+        }
+    };
+
+    const updateCart = () => {
+        const productIds = Object.keys(countMap);
+
+        document.querySelector(".cart-items").innerHTML = productIds
+            .map((productId) => {
+                const productInCart = productMap[productId];
+                if (countMap[productId] === 0) {
+                    return "";
+                }
+                return getProductHTML(productInCart, countMap[productId]);
+            })
+            .join("");
+
+        document.querySelector(".total_count").innerHTML = `(${sumAllCount(
+            countMap
+        )})`;
+    };
+
+    const increaseCount = (productId) => {
+        if (countMap[productId] === undefined) {
+            countMap[productId] = 0;
+        }
+        countMap[productId] += 1;
+        updateProductCount(productId);
+        updateCart();
+    };
+
+    const decreaseCount = (productId) => {
+        if (countMap[productId] === undefined) {
+            countMap[productId] = 0;
+        }
+         if (countMap[productId] > 0) {
+             countMap[productId] -= 1;
+        }
+            updateProductCount(productId);
+            updateCart();
+        
+    };
+
+    document.querySelector("#products").innerHTML = products
+        .map((product) => getProductHTML(product))
         .join("");
 
     document.querySelector("#products").addEventListener("click", (e) => {
         const targetElement = e.target;
         const productElement = findElement(targetElement, ".product");
         const productId = productElement.getAttribute("data-product-id");
-        console.log("productId", productId);
-        const productIndex = productElement.getAttribute("data-product-index");
-        const product = products[productIndex];
+        const product = productMap[productId];
+        // 위처럼 배열로 했을 때 배열이 너무 길면 느릴 수 있음.. productId를 키로해서 object 형채로 만들면 빨리 가져올 수 있음
         if (
             targetElement.matches(".btn-decrease") ||
             targetElement.matches(".btn-increase")
         ) {
-            console.log(countMap[productId]);
-            if (countMap[productId] === undefined) {
-                countMap[productId] = 0;
-            }
             if (targetElement.matches(".btn-decrease")) {
-                countMap[productId] -= 1;
+                decreaseCount(productId);
             } else if (targetElement.matches(".btn-increase")) {
-                countMap[productId] += 1;
+                increaseCount(productId);
             }
+        }
+    });
 
-            const cartCount = productElement.querySelector(".cart-count");
-            cartCount.innerHTML = countMap[productId];
-            if (countMap[productId] === 0) {
-                cartCount.innerHTML = "";
+    document.querySelector(".cart-items").addEventListener("click", (e) => {
+        const targetElement = e.target;
+        const productElement = findElement(targetElement, ".product");
+        const productId = productElement.getAttribute("data-product-id");
+        const product = productMap[productId];
+        // 위처럼 배열로 했을 때 배열이 너무 길면 느릴 수 있음.. productId를 키로해서 object 형채로 만들면 빨리 가져올 수 있음
+        if (
+            targetElement.matches(".btn-decrease") ||
+            targetElement.matches(".btn-increase")
+        ) {
+            if (targetElement.matches(".btn-decrease")) {
+                decreaseCount(productId);
+            } else if (targetElement.matches(".btn-increase")) {
+                increaseCount(productId);
             }
-
-            document.querySelector(".total_count").innerHTML = `(${sumAllCount(
-                countMap
-            )})`;
         }
     });
 
