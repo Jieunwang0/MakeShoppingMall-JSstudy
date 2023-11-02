78a81b415a281bec6251e0f4bf3bafeeb47868a1
jieunw <jieunwang00@gmail.com>
2023-11-02 15:22:50 +0900

    feat : ep04 완

--- ep04/src/reduce.test.js
+++ ep04/src/reduce.test.js
@@ -1,82 +1,111 @@
-import { describe, it, expect } from 'vitest';
-import { shows } from './data';
+import { describe, it, expect } from "vitest";
+import { shows } from "./data";
 
-describe('reduce method', () => {
-  it('calculates the total of an array', () => {
-    const numbers = [1, 2, 3, 4, 5];
+describe("reduce method", () => {
+    it.only("calculates the total of an array", () => {
+        const numbers = [1, 2, 3, 4, 5];
 
-    // TODO: do something here
-    const sum = 0;
-    expect(sum).toBe(15);
-  });
+        // TODO: do something here
+        const sum = numbers.reduce((sum, number) => {
+            sum += number;
+            return sum;
+        });
 
-  it('groups by genre', () => {
-    // TODO: do something with `shows` here
-    const groupedShows = [];
-    expect(groupedShows).toEqual({
-      Comedy: ["Don't Look Up"],
-      Drama: ['Stranger Things', 'Our Blues', 'Inventing Anna'],
-      Mistery: ["Dirk Gently's Holistic Detective Agency"],
-      Mystery: ['Little Women'],
+        expect(sum).toBe(15);
     });
-  });
 
-  it('groups by key (2)', () => {
-    // TODO: do something with `shows` here
-    const groupedShows = [];
-    expect(groupedShows).toEqual([
-      {
-        genre: 'Drama',
-        titles: ['Stranger Things', 'Our Blues', 'Inventing Anna'],
-      },
-      {
-        genre: 'Mystery',
-        titles: ['Little Women'],
-      },
-      {
-        genre: 'Comedy',
-        titles: ["Don't Look Up"],
-      },
-      {
-        genre: 'Mistery',
-        titles: ["Dirk Gently's Holistic Detective Agency"],
-      },
-    ]);
-  });
+    it.only("groups by genre", () => {
+        // TODO: do something with `shows` here
+        const callbackFn = (result, show) => {
+            if (!result[show.genre]) {
+                result[show.genre] = [];
+            }
+            result[show.genre].push(show.title);
+            return result;
+        };
+        const initialValue = {};
+        const groupedShows = shows.reduce(callbackFn, initialValue);
+        expect(groupedShows).toEqual({
+            Comedy: ["Don't Look Up"],
+            Drama: ["Stranger Things", "Our Blues", "Inventing Anna"],
+            Mistery: ["Dirk Gently's Holistic Detective Agency"],
+            Mystery: ["Little Women"],
+        });
+    });
 
-  it('flattens array', () => {
-    const nestedArray = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
+    it.only("groups by key (2)", () => {
+        // TODO: do something with `shows` here
+        const groupedShows = shows.reduce((result, show) => {
+            const index = result.findIndex((resultShow) => resultShow.genre === show.genre);
+            if (index === -1) {
+                result.push({
+                    genre: show.genre,
+                    titles: [show.title],
+                });
+            } else {
+                result[index].titles.push(show.title);
+            }
+            return result;
+        }, []);
+        expect(groupedShows).toEqual([
+            {
+                genre: "Drama",
+                titles: ["Stranger Things", "Our Blues", "Inventing Anna"],
+            },
+            {
+                genre: "Mystery",
+                titles: ["Little Women"],
+            },
+            {
+                genre: "Comedy",
+                titles: ["Don't Look Up"],
+            },
+            {
+                genre: "Mistery",
+                titles: ["Dirk Gently's Holistic Detective Agency"],
+            },
+        ]);
+    });
 
-    // TODO: do something here
-    const flatArray = [];
-    expect(flatArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  });
+    it.only("flattens array", () => {
+        const nestedArray = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
 
-  it('extracts writer names', () => {
-    // TODO: do something with `shows` here
-    const writerNames = [];
-    expect(writerNames).toEqual([
-      'Matt Duffer',
-      'Ross Duffer',
-      'Jessie Nickson-Lopez',
-      'Kate Trefry',
-      'Justin Doble',
-      'Alison Tatlock',
-      'Paul Dichter',
-      'Jessica Mecklenburg',
-      'Seo-Gyeong Jeong',
-      'Hee-kyung Noh',
-      'Shonda Rhimes',
-      'Carolyn Ingber',
-      'Jessica Pressler',
-      'Nicholas Nardini',
-      'Adam McKay',
-      'Max Landis',
-      'Douglas Adams',
-    ]);
-  });
+        // TODO: do something here
+        const flatArray = nestedArray.reduce((resultArray, arrayOfNumber) =>{
+        resultArray.push(...arrayOfNumber);
+        return resultArray;
+        },[])
+        expect(flatArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    it.only("extracts writer names", () => {
+        // TODO: do something with `shows` here
+        const writerNames = shows.reduce((names, show)=>{
+            names.push(...show.writers);
+            return names;  
+        }, [])
+        expect(writerNames).toEqual([
+            "Matt Duffer",
+            "Ross Duffer",
+            "Jessie Nickson-Lopez",
+            "Kate Trefry",
+            "Justin Doble",
+            "Alison Tatlock",
+            "Paul Dichter",
+            "Jessica Mecklenburg",
+            "Seo-Gyeong Jeong",
+            "Hee-kyung Noh",
+            "Shonda Rhimes",
+            "Carolyn Ingber",
+            "Jessica Pressler",
+            "Nicholas Nardini",
+            "Adam McKay",
+            "Max Landis",
+            "Douglas Adams",
+        ]);
+    });
 });
